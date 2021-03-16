427d06d4d8ff720f97d06e40c9f64aca3605f7f3
HO LAM <20020201@vnu.edu.vn>
2021-03-16 09:17:03 +0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
-
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <bits/stdc++.h>
 
 using namespace std;
-const int SCREEN_WIDTH = 800;
+const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 600;
 SDL_Window* window = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Renderer* renderer = NULL;
+SDL_Surface* g_Object = NULL;
+int k=0;
 void init()
 {
    TTF_Init();
@@ -16,12 +17,26 @@ void init()
     renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
 
 }
+void render_map(SDL_Renderer *renderer,string path,SDL_Rect rect)
+{
+    SDL_Surface *surface = SDL_LoadBMP(path.c_str());
+    SDL_Texture *texture = SDL_CreateTextureFromSurface( renderer, surface);
+    SDL_Rect ScrR;
+    ScrR.x=k;
+    ScrR.y=0;
+    ScrR.w=SCREEN_WIDTH;
+    ScrR.h=SCREEN_HEIGHT;
+    SDL_RenderCopy( renderer, texture, &ScrR, &rect );
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
+
+}
 void render_image(SDL_Renderer *renderer,string path,SDL_Rect rect)
 {
 
     SDL_Surface *surface = SDL_LoadBMP(path.c_str());
     SDL_Texture *texture = SDL_CreateTextureFromSurface( renderer, surface);
-    SDL_RenderCopy( renderer, texture, NULL, &rect );
+    SDL_RenderCopy( renderer, texture, NULL , &rect );
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(surface);
 
@@ -42,10 +57,21 @@ int main(int argc, char* args[])
 {
     init();
     SDL_RenderClear(renderer);
-    SDL_Rect r={400,0,SCREEN_WIDTH,SCREEN_HEIGHT};
-    render_image(renderer,"image/a.bmp",r);
-    r={600,300,200,100};
-    render_text(renderer,"ccccc",r,20);
+    int dem=0;
+    while (k<=SCREEN_WIDTH)
+    {
+    SDL_Rect r={0,0,SCREEN_WIDTH,SCREEN_HEIGHT};
+    render_map(renderer,"image/map.bmp",r);
+    r={100,450,54,85};
+    if (dem%3==0) render_image(renderer,"image/zom1.bmp",r);
+    if (dem%3==1) render_image(renderer,"image/zom2.bmp",r);
+    if (dem%3==2) render_image(renderer,"image/zom3.bmp",r);
+
     SDL_RenderPresent(renderer);
-    SDL_Delay(10000);
+    dem++;
+    k+=8;
+    if (k==SCREEN_WIDTH) k=0;
+    SDL_Delay(100);
+    }
+    SDL_Delay(5000);
 }
